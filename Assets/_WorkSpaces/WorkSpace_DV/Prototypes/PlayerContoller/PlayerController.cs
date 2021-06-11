eeb2766fbddec2ade5d6717cf8398f591d415871
brubow@newvisionsproject.de <brubow@newvisionsproject.de>
2021-06-12 01:57:47 +0200

    Added Prefabs

--- Assets/_WorkSpaces/WorkSpace_DV/Prototypes/PlayerContoller/PlayerController.cs
+++ Assets/_WorkSpaces/WorkSpace_DV/Prototypes/PlayerContoller/PlayerController.cs
@@ -1,38 +1,37 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{ 
-    public GameObject player1;
+{
+        public GameObject player1;
     public GameObject player2;
     public float horizontalInputP1;
     public float verticalInputP1;
     public float horizontalInputP2;
     public float verticalInputP2;
-    public float speedP1 = 1f;
-    public float speedP2 = 1f;
+    public float speedP1;
+    public float speedP2;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // Player 1
-        horizontalInputP1 = Input.GetAxis("Horizontal");
+        horizontalInputP1 = Input.GetAxis("Horizontal1");
         player1.transform.Translate(Vector3.right * horizontalInputP1 * Time.deltaTime * speedP1);
-        verticalInputP1 = Input.GetAxis("Vertical");
-        player1.transform.Translate(Vector3.up * verticalInputP1 * Time.deltaTime * speedP1);
+        verticalInputP1 = Input.GetAxis("Vertical1");
+        player1.transform.Translate(Vector3.forward * verticalInputP1 * Time.deltaTime * speedP1);
 
-        // Player 1
-        horizontalInputP2 = Input.GetAxis("Horizontal");
-        player1.transform.Translate(Vector3.right * horizontalInputP2 * Time.deltaTime * speedP2);
-        verticalInputP2 = Input.GetAxis("Vertical");
-        player1.transform.Translate(Vector3.up * verticalInputP2 * Time.deltaTime * speedP2);
+        // Player 2
+        horizontalInputP2 = Input.GetAxis("Horizontal2");
+        player2.transform.Translate(Vector3.right * horizontalInputP2 * Time.deltaTime * speedP2);
+        verticalInputP2 = Input.GetAxis("Vertical2");
+        player2.transform.Translate(Vector3.forward * verticalInputP2 * Time.deltaTime * speedP2);
     }
 }
