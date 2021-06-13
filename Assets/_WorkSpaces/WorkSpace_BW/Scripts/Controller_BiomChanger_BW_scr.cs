f16e3afbba1493b7a1e082f2b214d344773af12e
Storbird <benjaminweithaus@live.com>
2021-06-13 13:12:58 +0200

    BiomTrigger Test

---
+++ Assets/_WorkSpaces/WorkSpace_BW/Scripts/Controller_BiomChanger_BW_scr.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Nvp.Events;
+
+public class Controller_BiomChanger_BW_scr : MonoBehaviour
+{
+    [SerializeField] private string _biomEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EventManager.Invoke(_biomEvent, this, null);
+        Debug.Log("OnTriggerEnter");
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        EventManager.Invoke("OnGrandpa", this, null);
+        Debug.Log("OnTriggerExit");
+    }
+}
